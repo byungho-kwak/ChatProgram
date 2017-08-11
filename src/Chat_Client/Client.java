147c65014466d324bcc0e64c8f6f6c61f2d43b78
kwak <byunghokwak5497@gmail.com>
2017-08-11 21:03:50 +0900

    1. 접속자 리스트 2. 쪽지 보내기 기능

---
+++ src/Chat_Client/Client.java
@@ -0,0 +1,287 @@
+package Chat_Client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class Client extends JFrame implements ActionListener{
+	
+	//로그인 GUI 변수
+	private JFrame Login_GUI = new JFrame();
+	private JPanel login_pane;
+	private JTextField ip_tf;	// ip 받는 텍스트필드
+	private JTextField port_tf; // port 받는 텍스트필드
+	private JTextField id_tf; // id 받는 텍스트필드
+	private JButton login_btn = new JButton("접 속");
+	
+	// Main GUI 변수
+	private JPanel main_pane;
+	private JTextField message_tf;
+	private JButton notesend_btn = new JButton("쪽지 보내기");
+	private JButton joinroom_btn = new JButton("채팅방 참여");
+	private JButton createroom_btn = new JButton("방 만들기");
+	private JButton sendmessage_btn = new JButton("전송");
+	
+	private JList user_list = new JList();	//전체접속자 리스트
+	private JList room_list = new JList();	// 채팅방 목록 리스트
+	private JTextArea chat_area = new JTextArea(); // 채팅창 변수
+	
+	
+	// 네트워크를 위한 자원 변수
+	private Socket socket;
+	private String ip;
+	private int port;
+	private String id;
+	
+	// 스트림 자원 생성
+	private InputStream is;
+	private OutputStream os;
+	private DataInputStream dis;
+	private DataOutputStream dos;
+	
+	// 그 외 변수들
+	Vector User_List = new Vector();
+	Vector Room_List = new Vector();
+	// 서버로 부터 전달되어온 데이터 파싱해서 어떤 데이터인지 확인하기 위한 용도
+	StringTokenizer st;
+	
+	// 생성자 메소드
+	Client() {	
+		Login_init();
+		Main_init();
+		start();
+	}
+	
+	private void start() {
+		login_btn.addActionListener(this);	// 로그인 버튼 리스너
+		notesend_btn.addActionListener(this);	// 쪽지 보내기 리스너
+		joinroom_btn.addActionListener(this);	// 채팅방 참여 리스너
+		createroom_btn.addActionListener(this);	// 채팅방 만들기 리스너
+		sendmessage_btn.addActionListener(this);	// 채팅 전송 리스너
+	}
+	
+	private void Main_init() {
+		// 아래 메소드들의 맨 앞에 this가 생략되어 있음 = JFrame 자체를 상속 받았기 떄문에
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 679, 572);
+		main_pane = new JPanel();
+		main_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(main_pane);
+		main_pane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("전 체 접 속 자");
+		lblNewLabel.setBounds(12, 26, 129, 15);
+		main_pane.add(lblNewLabel);
+		
+		user_list.setBounds(12, 51, 129, 144);
+		main_pane.add(user_list);
+
+		notesend_btn.setBounds(12, 205, 129, 23);
+		main_pane.add(notesend_btn);
+		
+		JLabel label = new JLabel("채 팅 방 목 록");
+		label.setBounds(12, 260, 129, 15);
+		main_pane.add(label);
+		
+		room_list.setBounds(12, 285, 129, 139);
+		main_pane.add(room_list);
+		
+		joinroom_btn.setBounds(12, 434, 129, 23);
+		main_pane.add(joinroom_btn);
+		
+		createroom_btn.setBounds(12, 467, 129, 23);
+		main_pane.add(createroom_btn);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(160, 47, 487, 398);
+		main_pane.add(scrollPane);
+		
+		scrollPane.setViewportView(chat_area);
+		
+		message_tf = new JTextField();
+		message_tf.setBounds(160, 468, 378, 21);
+		main_pane.add(message_tf);
+		message_tf.setColumns(10);
+		
+		sendmessage_btn.setBounds(550, 467, 97, 23);
+		main_pane.add(sendmessage_btn);
+		
+		this.setVisible(true);
+	}
+	private void Login_init() {
+	
+		// 하지만 로그인 관련 창에 대해서는 객체를 따로 생성했기 때문에 맨 앞에 Login_GUI 객체명으로 넣어준다.
+		Login_GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		Login_GUI.setBounds(100, 100, 295, 400);
+		login_pane = new JPanel();
+		login_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		Login_GUI.setContentPane(login_pane);
+		login_pane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Server IP");
+		lblNewLabel.setBounds(29, 150, 78, 15);
+		login_pane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Server Port");
+		lblNewLabel_1.setBounds(29, 201, 78, 15);
+		login_pane.add(lblNewLabel_1);
+		
+		JLabel lblId = new JLabel("ID");
+		lblId.setBounds(29, 256, 78, 15);
+		login_pane.add(lblId);
+		
+		ip_tf = new JTextField();
+		ip_tf.setBounds(119, 147, 116, 21);
+		login_pane.add(ip_tf);
+		ip_tf.setColumns(10);
+		
+		port_tf = new JTextField();
+		port_tf.setBounds(119, 198, 116, 21);
+		login_pane.add(port_tf);
+		port_tf.setColumns(10);
+		
+		id_tf = new JTextField();
+		id_tf.setBounds(119, 253, 116, 21);
+		login_pane.add(id_tf);
+		id_tf.setColumns(10);
+		
+		
+		login_btn.setBounds(29, 304, 206, 23);
+		login_pane.add(login_btn);
+		
+		Login_GUI.setVisible(true); // true = 화면에 보이게	false = 화면에 보이지 않게
+		
+	}
+	
+	// 소켓생성 및 커넥션 메소드(스트림/메시지 송신 스레드 생성) 호출
+	private void Network() {
+		try {
+			socket = new Socket(ip, port);
+			// 정상적으로 연결 됐을 때 스트림 설정하기
+			if(socket!=null) {
+				Connection();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	public static void main(String[] args) {
+		new Client();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == login_btn) {
+			
+			ip = ip_tf.getText().trim();	// trim : 맨 뒤에 빈 공간이 생겼을 때 앞으로 댕겨주는 메소드
+			port = Integer.parseInt(port_tf.getText().trim());	// int형 형변환
+			id = id_tf.getText().trim();  // ID 값 저장
+			System.out.println("로그인버튼 클릭");		
+			Network();	
+		}
+		else if(e.getSource() == notesend_btn) {
+			System.out.println("쪽지 보내기 버튼 클릭");
+			String user = (String)user_list.getSelectedValue();
+			
+			// 쪽지 보내기 다이알로그 생성
+		}
+		else if(e.getSource() == joinroom_btn)
+			System.out.println("방 참여 버튼 클릭");
+		else if(e.getSource() == createroom_btn)
+			System.out.println("방 만들기 버튼 클릭");
+		else if(e.getSource() == sendmessage_btn) {
+			send_message("임시테스트 입니다");
+			System.out.println("채팅 전송 버튼 클릭");
+		}
+	}
+
+	private void Connection() {
+		
+		// 서버로의 in/out 스트림 설정
+		try {
+			is = socket.getInputStream();
+			os = socket.getOutputStream();
+			dis = new DataInputStream(is);
+			dos = new DataOutputStream(os);
+		} catch(IOException e) {
+			
+		} // Stream 설정 끝
+		
+		// 처음 접속 시 ID 받아서 서버로 전달 send_message 사용
+		send_message(id);
+		
+		User_List.add(id); // Vector에 id 저장
+		
+		// 입력(서버로부터 메시지 수신)을 위한 스레드 생성(익명 클래스)
+		Thread th = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(true) {
+					try {
+						String msg = dis.readUTF();
+						System.out.println("서버로부터 수신된 메시지: "+msg);
+						in_message(msg);
+						
+					} catch (IOException e) {
+					}
+				}
+			}
+		});
+		th.start();
+		
+		//User 리스트에 사용자 추가
+
+	}
+	
+	// 서버로부터 들어오는 모든 메시지
+	private void in_message(String str) {
+		st = new StringTokenizer(str, "/");
+		
+		String protocol = st.nextToken();
+		String message = st.nextToken();
+		
+		if(protocol.equals("NewUser")) {
+			User_List.add(message);
+			user_list.setListData(User_List);
+		}
+		
+		if(protocol.equals("ExistingUser")) {
+			if(message.equals("End"))
+				user_list.setListData(User_List);
+			else
+				User_List.add(message);
+		}
+	
+	}
+	
+	// 서버로 메시지 보내는 메소드
+	private void send_message(String str) {
+		try {
+			dos.writeUTF(str);
+		} catch (IOException e) {
+			
+		}
+	}
+}
---
+++ src/Chat_Server/Server.java
@@ -0,0 +1,235 @@
+package Chat_Server;
+
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class Server extends JFrame implements ActionListener{
+	
+	private JPanel contentPane;
+	private JTextField port_tf;
+	private JTextArea textArea = new JTextArea();
+	private JButton start_btn = new JButton("서버 실행");
+	private JButton stop_btn = new JButton("서버 중지");
+	
+	//Network 자원 생성
+	private ServerSocket server_socket;
+	private Socket socket;
+	private int port;
+	private StringTokenizer st;
+	
+	private Vector user_vc = new Vector<>();
+	
+	Server() {	// 생성자 
+		init();	// 화면 생성 메소드
+		start();	// 리스너 설정 메소드
+	}
+	
+	private void start() {
+		// start, stop버튼에 대한 actionListner 설정하기
+		start_btn.addActionListener(this);	// 자체 클래스에서 actionListner를 상속받았기 때문에 this
+		stop_btn.addActionListener(this);	
+		
+	}
+
+	// 화면 구성
+	private void init() {	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 384, 414);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 10, 344, 243);
+		contentPane.add(scrollPane);
+		
+		
+		scrollPane.setViewportView(textArea);
+		
+		JLabel lblNewLabel = new JLabel("포트 번호");
+		lblNewLabel.setBounds(12, 275, 57, 15);
+		contentPane.add(lblNewLabel);
+		
+		port_tf = new JTextField();
+		port_tf.setBounds(81, 272, 275, 21);
+		contentPane.add(port_tf);
+		port_tf.setColumns(10);
+		
+		start_btn.setBounds(12, 321, 161, 23);
+		contentPane.add(start_btn);
+		
+		stop_btn.setBounds(188, 321, 168, 23);
+		contentPane.add(stop_btn);
+		
+		this.setVisible(true); // true = 화면에 보이게	false = 화면에 보이지 않게
+	}
+	private void Server_start() {
+		try {
+			server_socket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// 정상적으로 포트가 열렸을 경우
+		if(server_socket != null) {
+			Connection();
+		}
+	}
+	
+	private void Connection() {
+		// 1가지의 스레드에서는 1가지의 일만 처리할 수 있다.
+		// 소켓 accept로 무한대기일 때는 다른 버튼 누를 수 없는 상태가 된다.
+		// 이를 위해 accpet부분 thread 추가해서 처리
+		
+		// 무한루프틑 통한 다중 사용자 받기(스레드 생성)
+	
+		Thread th = new Thread(new Runnable() {
+			@Override
+			public void run() {	// 스레드에서 처리할 일을 기재
+				while(true) {
+					// 사용자 접속 대기 무한대
+					try {
+						
+						textArea.append("사용자 접속 대기중\n");
+						socket = server_socket.accept();
+						textArea.append("사용자 접속!\n");
+						
+						UserInfo user = new UserInfo(socket);
+						user.start();
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		th.start();
+	}
+	
+	public static void main(String[] args) {
+		
+		new Server();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == start_btn) {
+			port = Integer.parseInt(port_tf.getText().trim());
+			System.out.println("서비스 스타트 버튼 클릭");
+			Server_start();	// 소켓생성 및 사용자 접속 대기
+		}
+		
+		if(e.getSource() == stop_btn)
+			System.out.println("서비스 중지 버튼 클릭");
+	}	// 액션 이벤트 끝
+	
+	// 사용자 각각의 스레드를 만들어줘야 하기 때문에, 스레드를 상속받은 내부 클래스로 만들어준다. 
+	class UserInfo extends Thread {
+		
+		// 스트림 자원 생성
+		private InputStream is;
+		private OutputStream os;
+		private DataInputStream dis;
+		private DataOutputStream dos;
+		
+		private Socket user_socket;
+		private String NicName;
+		
+		UserInfo(Socket soc) {
+			this.user_socket = soc;
+			UserNetWork();
+		}
+		
+		private void UserNetWork() {
+			try {
+				is = user_socket.getInputStream();
+				dis = new DataInputStream(is);
+				os = user_socket.getOutputStream();
+				dos = new DataOutputStream(os);
+				
+				NicName = dis.readUTF();
+				textArea.append(NicName+" : 사용자 접속!\n");
+				
+				System.out.println("현재 접속된 사용자 수: "+user_vc.size());
+				
+				// 새로 추가된 user에게 기존 user 모두 전송 후 -프로토콜 : (Existing/User1~N)
+				BroadCase("NewUser/"+NicName);
+				
+				for(int i=0; i<user_vc.size(); i++) {
+					UserInfo u = (UserInfo)user_vc.elementAt(i);
+					send_message("ExistingUser/"+u.NicName);
+				}
+				//클라이언트에게 vector 모두 다 보내줬음을 알려주기 위한 프로토콜(ExistingUser/End)
+				send_message("ExistingUser/"+"End");
+				//vector에 자신 등록
+				user_vc.add(this);
+				
+				
+			} catch (IOException e) {
+			
+			}
+		}
+		
+		public void run() {
+			while(true) {
+				try {
+					String msg = dis.readUTF();
+					textArea.append(NicName+"사용자로부터 들어온 메시지 :"+msg+"\n");
+					
+					in_message(msg);
+					
+					
+				} catch (IOException e) {
+				}
+			}
+		} // run 메소드 끝
+		
+		//서버로 들어오는 메시지 처리
+		private void in_message(String str) {
+			
+			// 쪽지로 전송된 프로토콜/사용자/내용 토크나이저 통한 파싱
+			
+		}
+		
+		private void send_message(String str) {
+			
+			try {
+				dos.writeUTF(str);
+			} catch (IOException e) {
+			}
+			
+		}
+		
+		// 전체 사용자에게 메시지 전송
+		private void BroadCase(String str) {
+			// 기존(현재) 사용자들에게 새로운 user 접속 알림 
+			for(int i=0; i<user_vc.size(); i++) {
+				UserInfo u = (UserInfo)user_vc.elementAt(i);
+				u.send_message(str);
+			}
+		}
+	}
+
+}
